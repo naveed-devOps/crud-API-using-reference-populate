43fd8b94b93f09b6feea52ec806bde04ad2f0dfa
Muhammad Naveed <naveed65dev@gmail.com>
2023-12-20 22:11:30 +0500

    update countryRoute.js

--- api/routes/countryRoute.js
+++ api/routes/countryRoute.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const countryController = require('../controllers/countryController');
- 
 
 // Routes for countryController
-router.get('/country', countryController.getAllCountry);
-router.get('/country/id/:id', countryController.getCountryById); 
-// Routes for create a country
-router.post('/country', countryController.createCountry);
-// Routes for delete a country
-router.delete('/country/id/:id', countryController.deletedCountryById); 
-// Routes for update a country
-router.put('/country/id/:id', countryController.updatedCountryById); 
- 
+router.get('/', countryController.getAllCountry);
+router.get('/:id', countryController.getCountryById);
+router.post('/', countryController.createCountry);
+router.delete('/:id', countryController.deletedCountryById);
+router.put('/:id', countryController.updatedCountryById);
 
 module.exports = router;
